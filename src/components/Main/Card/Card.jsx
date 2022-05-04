b6d36a3e4d967168939b19c73ced4e13540b5ff1
Alexdelaoc <alexdelaoc@gmail.com>
2022-05-04 23:36:26 +0200

    v1.0, missing styles

--- src/components/Main/Card/Card.jsx
+++ src/components/Main/Card/Card.jsx
@@ -1,9 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Card extends Component {
-  render() {
-    return <div>Card</div>;
-  }
+const Card = (props) => {
+  return(
+    <section className="card">
+      <h4>{props.data.name}</h4>
+      <div className="card__sprites">
+        <div>
+          <p>Default form:</p>
+          <img src={props.data.sprites.front_default} alt={props.data.name} />
+        </div>
+        <div>
+          <p>Shiny form:</p>
+          <img src={props.data.sprites.front_shiny} alt={props.data.name} />
+        </div>
+      </div>
+      <div className="card__types">
+        <p>Type:</p>
+        <p>{props.data.types[0].type.name}</p>
+        { props.data.types[1] ? <p> {props.data.types[1].type.name} </p> : [] } 
+      </div>
+    </section>
+  )
 }
 
 export default Card;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,60 @@
-import React, { Component } from "react";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import Card from './Card'
 
-class Main extends Component {
-  render() {
-    return <div>Main</div>;
+const Main = () => {
+
+  // Creamos los estados
+  const [input, setInput] = useState('');
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        try {
+          if (input !== '') {
+            const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
+            const data = await resp.data;
+            setPokemon([data]); // setPokemon([data, ...pokemon]) si quiero que se sigan almacenando en Pokemons en el array del estado, y seguir pintándolos.
+            setInput('');
+          }
+        } catch (error) {
+          console.log(error);
+          setInput('');
+        }
+      };
+      fetchData();
+    },
+    [input] 
+    // eslint-disable-line react-hooks/exhaustive-deps
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInput(e.target.input.value.toLowerCase());
+    e.target.input.value = '';
+  };
+
+  const paintCards = () => {
+    return pokemon.map(aPokemon => (
+      <Card key={uuidv4()} data={aPokemon} />
+    ));
   }
+
+  return (
+    <div>
+      <h1>Welcome to your Pokedex!</h1>
+      <h3>This is a project, developed with React and with help from:</h3>
+      <img src="https://avatars.githubusercontent.com/u/64151210?v=4" alt="pokeApi" />
+
+      <form onSubmit={handleSubmit}>
+        <input name="input" />
+        <button type="submit"> Search </button>
+      </form>
+      { pokemon.length !== 0 ? paintCards() : "" }
+    </div>
+  )
 }
 
 export default Main;
