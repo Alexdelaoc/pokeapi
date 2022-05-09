2241d2338442546e02d8b9862c3be34d1c03dd97
Alexdelaoc <alexdelaoc@gmail.com>
2022-05-09 02:50:49 +0200

    added styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,13 +1,15 @@
 import axios from "axios";
 import React, { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import Card from './Card'
+import PokeCard from './PokeCard';
+import { TextField, Typography, LinearProgress } from "@mui/material";
 
 const Main = () => {
 
   // Creamos los estados
   const [input, setInput] = useState('');
   const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(
     () => {
@@ -17,10 +19,14 @@ const Main = () => {
             const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${input}`);
             const data = await resp.data;
             setPokemon([data, ...pokemon]);
+            setLoading(false)
+          } else if (input === "") {
+            setLoading(false)
           }
         } catch (error) {
           console.log(error);
           setInput('');
+          setLoading(false);
         }
       };
       fetchData();
@@ -33,33 +39,41 @@ const Main = () => {
       const context = this; // Teniendo en cuenta el comportamiento de la función de setTimeout (la cuál crea una nueva instancia en el objeto cada vez que se llama, aplicamos a la constante context la instancia en la que se le está aplicando, para posteriormente aplicárselo como argumento a la función de callback).
       clearTimeout(timeout); // Si timeout está definido, servirá como argumento para clearTimeout.
       timeout = setTimeout(() => callback.apply(context, args), wait); // El wait es innceseraio, ya que setTimeout() de por sí acepta como segundo argumento un número que significa el tiempo a esperar en milisegundos.
+      setLoading(true);
     };
   }
 
   const handleChange = (e) => {
     if (e.target.value !== '') {
-      console.log(e.target.value);
-      setInput(e.target.value.toLowerCase());
+      setInput(e.target.value.toLowerCase()); // Hay que asegurarse de que el Pokemon que se introduce en el estado pokemon no está repetido.
     }
     
   }
 
   const paintCards = () => {
     return pokemon.map(aPokemon => (
-      <Card key={uuidv4()} data={aPokemon} />
+      <PokeCard key={uuidv4()} data={aPokemon} />
     ));
   }
 
   return (
-    <div>
-      <h1>Welcome to your Pokedex!</h1>
-      <h3>This is a project developed with React and with help from:</h3>
-      <img src="https://avatars.githubusercontent.com/u/64151210?v=4" alt="pokeApi" />
-
-      <form>
-        <input type="text" name="input" placeholder="Gotta catch 'em all!" onChange={ debounce(handleChange, 2000)}/>
-      </form>
-      {pokemon.length !== 0 ? paintCards() : ""}
+    <div className="main">
+      <div className="main__presentation">
+        <Typography variant="h3">
+          Welcome to your Pokedex!
+        </Typography>
+        <Typography variant="h4">
+          This is a project developed with React for The Bridge
+        </Typography>
+      </div>
+      <div className="main__searchbar">
+        <TextField id="filled-basic" fullWidth label="Gotta catch 'em all!" variant="filled" name="input" onChange={debounce(handleChange, 3000)}/>
+      </div>
+      {loading ? <div className="main__loading-bar"> <LinearProgress/> </div> : ''}
+      <div className="main__pokeList">
+        {pokemon.length !== 0 ? paintCards() : ""}
+      </div>
+      
 
     </div>
   )
